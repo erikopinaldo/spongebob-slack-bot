895c3dcbb83d83b2cf88b7c518e4ca8ff4ba2a50
Erik Opinaldo <erikopinaldo@gmail.com>
2022-08-25 22:38:14 -0400

    intial commit

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,17 @@
 module.exports = {
-    getIndex: (req,res)=>{
-        res.render('index.ejs')
+    postSpongebob: (req,res)=>{
+        let textArr = req.body.text.toLowerCase().split('')
+        let sponge = textArr.map(letter => {
+            let random = Math.round(Math.random())
+
+            return random < 0.5 ? letter : letter.toUpperCase()  
+        })
+        .join('')
+        
+        console.log(sponge)
+
+        res.json({ text: sponge })
+
+
     }
 }
--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,6 @@ const express = require('express')
 const router = express.Router()
 const homeController = require('../controllers/home')
 
-router.get('/', homeController.getIndex) 
+router.post('/', homeController.postSpongebob) 
 
 module.exports = router
