8b488118db111b2ad3a39695c00d0625443b0cde
Erik Opinaldo <erikopinaldo@gmail.com>
2022-08-29 19:34:20 -0400

    require verifySignature

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const signature = require('./verifySignature');
 const bodyParser = require('body-parser')
 const homeRoutes = require('./routes/home')
 const installRoutes = require('./routes/install')
