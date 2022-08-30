252aa717295794d51c4148bc50847225ddf6ddd5
Erik Opinaldo <erikopinaldo@gmail.com>
2022-08-29 20:01:35 -0400

    added logging for headers

--- verifySignature.js
+++ verifySignature.js
@@ -1,14 +1,16 @@
 const crypto = require('crypto');
 
 const isVerified = (req) => {
-    
+
     // req.headers is an object
     const signature = req.headers['x-slack-signature'];
     const timestamp = req.headers['x-slack-request-timestamp'];
-    
+
     // Create Hmac object that uses the sha256 crytographic hashing algorithm. Our signing secret acts as the key to this algorithm so that we can compare against the Slack signature we receive
     const hmac = crypto.createHmac('sha256', process.env.SLACK_SIGNING_SECRET);
-    
+
+    console.log(req.headers)
+
     // The signature variable from our req header will be split into two different variables, the version and the hash. More information about these two different parts at https://api.slack.com/authentication/verifying-requests-from-slack#verifying-requests-from-slack-using-signing-secrets__a-recipe-for-security__how-to-make-a-request-signature-in-4-easy-steps-an-overview
     const [version, hash] = signature.split('=');
 
